3e3c379e9c5cc6c791f9a8c2516b306d6260eb20
Iuli Dercaci <iuli.dercaci@site-me.info>
2012-11-10 22:05:21 +0200

    Uploading file functionality, added Excel parser usage

--- dev_stopfraud_tst_zf/application/controllers/AddController.php
+++ dev_stopfraud_tst_zf/application/controllers/AddController.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * uploading Excel file, parsing it into an array,
+ * sorting and assembling resulting data for the DB
+ * @author Iuli Dercaci <iuli.dercaci@site-me.info>
+ */
 class AddController extends Zend_Controller_Action
 {
 
@@ -13,12 +17,30 @@ class AddController extends Zend_Controller_Action
         $mapper = new Application_Model_PhonezoneMapper();
         $form = new Application_Form_AddInfo(array('regions' => $mapper->fetchFormSelectOptions()));
 
+        if ($this->_request->isPost()){
+            $formData = $this->_request->getPost();
+            if ($form->isValid($formData)){
+                $upload = new Zend_File_Transfer_Adapter_Http();
+                try{
+                    $upload->receive();
+                } catch (Zend_File_Transfer_Exception $e) {
+                    echo $e->message();
+                }
+                //resulting file contents
+                $parser = new Excel_Parcer($upload->getFileName('upload'));
+                $data = $parser->toArray(true);
+                $this->forward('add',null,null,array('data' => $data));
+            } else {
+                //data is not valid
+                $form->populate($formData);
+            }
+        }
         $this->view->form = $form;
     }
 
     public function addAction()
     {
-        // action body
+        Zend_Debug::dump($this->_request->getParam('data'));
     }
 
 
--- dev_stopfraud_tst_zf/application/forms/AddInfo.php
+++ dev_stopfraud_tst_zf/application/forms/AddInfo.php
@@ -1,31 +1,34 @@
 <?php
-
+/**
+ * uploading Excel file form
+ * @author Iuli Dercaci <iuli.dercaci@site-me.info>
+ */
 class Application_Form_AddInfo extends Zend_Form
 {
     protected $_regions = array();
 
     public function init()
     {
-        $this->setMethod('post');
-        $this->addElement('select', 'regions', array(
-            'label'    => 'Region:',
-            'required' => true,
-            'multioptions' => $this->getRegions()
-        ));
-        $this->addElement('file', 'upload', array(
-            'label' => 'File:',
-            'required' => true,
-            'validators' => array(array('Count', false, 1), array('Size', false, 102400), array('Extension', false, 'xls,odt'))
-//            'destination' => $this->directory,
-
-        ));
-        $this->addElement('submit', 'submit', array(
-            'ignore' => true,
-            'label'  => 'Submit info'
-        ));
-        $this->addElement('hash', 'csrf', array(
-            'ignore' => true
-        ));
+        $this->setMethod('post')
+            ->setAttrib('enctype', 'multipart/form-data')
+            ->addElement('select', 'regions', array(
+                'label'    => 'Region:',
+                'required' => true,
+                'multioptions' => $this->getRegions()
+            ))
+            ->addElement('file', 'upload', array(
+                'label' => '    File:',
+                'required' => true,
+                'validators' => array(array('Count', false, 1), array('Size', false, 102400000), array('Extension', false, 'xls,xlsx,odf,csv')),
+                'destination' => APPLICATION_PATH . '/../uploads'
+            ))
+            ->addElement('submit', 'submit', array(
+                'ignore' => true,
+                'label'  => 'Submit info'
+            ))
+            ->addElement('hash', 'csrf', array(
+                'ignore' => true
+            ));
     }
 
     public function setRegions($regions) {
